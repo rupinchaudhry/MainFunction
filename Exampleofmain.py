81338306d8782260e097bfe2c135eaa0a3c0df9b
rupinchaudhry <briggenrupin@gmail.com>
2024-11-16 18:32:14 +0530

    Main function in python
    Main function in python

---
+++ Exampleofmain.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def main1():
+    print("I am main one")
+    a=datetime.now().time().hour
+    print(a)
+
+def main():
+    print("I am main")
+
+if __name__=="__main__":
+    main()
+
+main1()
